98a7b339958c8307587fd6dcafd3f99d5fc42328
magesh112 <114233098+magesh112@users.noreply.github.com>
2022-11-11 16:21:05 +0530

    reactive form

--- src/app/authentication/login/login.component.ts
+++ src/app/authentication/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  form=new FormGroup({
+    email:new FormControl('',[Validators.required,Validators.email,Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')]),
+    password:new FormControl('',[Validators.required,Validators.minLength(6)])
+  });
+
+  constructor(private route:Router) { }
 
   ngOnInit() {
+  }
+  get email(){
+    return this.form.get('email')
+  }
+  get password(){
+    return this.form.get('password')
+  }
+  submitFunction(){
+    this.route.navigate(['/home']);
   }
 
 }
